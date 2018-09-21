33aecef83da1fead7d5ec94e98021935ea488ec9
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-09-21 11:46:11 -0300

    criação do objeto NewEmp

---
+++ users/z_Emp_c0_obj.py
@@ -0,0 +1,20 @@
+class NewEmp_Object:
+    def __init__(self):
+        self.emp_nome        = ''
+        self.empdata_cnpj    = ''
+        self.empdata_email   = ''
+        self.empdata_razao   = ''
+        self.empdata_resp    = ''
+        self.empdata_tel     = ''
+
+        self.end_bairro      = ''
+        self.end_cep         = ''
+        self.end_cidade      = ''
+        self.end_comp        = ''
+        self.end_num         = ''
+        self.end_ref         = ''
+        self.end_rua         = ''
+        self.end_uf          = ''
+
+        self.cont_On         = '0'
+        self.cont_Tot        = '0'
