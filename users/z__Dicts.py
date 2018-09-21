078c35513e712ce7cb4a291526c36b69f3ae0c40
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-09-21 11:51:15 -0300

    dicionario de respostas

---
+++ users/z__Dicts.py
@@ -0,0 +1,16 @@
+Dict_respostas_Post = {
+    'inicializado':      0,
+    'sucesso':           1,
+    'ja_existe':         2,
+    'falhahistoryUsr':   3,
+    'falhahistory':      3,
+    'falha':             4     
+}
+
+Dict_respostas_Session = {
+    'inicializado':      0,
+    'sucesso':           1,
+    'sessaoInvalida':    2,
+    'acessoNegado':      3,
+    'erroDesconhecido':  4
+}
