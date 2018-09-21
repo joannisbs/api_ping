1fa35d3bcf18fec1f43b8c7d2b50be574c30e9ec
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-09-21 11:45:35 -0300

    criado funcoes para criacao de validadores de todos os dados rel a emp

---
+++ users/z_Emp_c0_Validators.py
@@ -0,0 +1,80 @@
+Dict_Validators = {
+    'emp_nome'     : Validate_emp_nome,
+    'empdata_cnpj' : Validate_empdata_cnpj,
+    'empdata_email': Validate_empdata_email,
+    'empdata_razao': Validate_empdata_razao,
+    'empdata_resp' : Validate_empdata_resp,
+    'empdata_tel'  : Validate_empdata_tel,
+
+    'end_bairro'   : Validate_end_bairro,
+    'end_cep'      : Validate_end_cep,
+    'end_cidade'   : Validate_end_cidade,
+    'end_comp'     : Validate_end_comp,
+    'end_num'      : Validate_end_num,
+    'end_ref'      : Validate_end_ref,
+    'end_rua'      : Validate_end_rua,
+    'end_uf'       : Validate_end_uf,
+}
+
+
+
+def Validate():
+    pass
+
+
+
+
+def Validate_emp_nome():
+    pass
+
+
+def Validate_empdata_cnpj():
+    pass
+
+
+def Validate_empdata_email():
+    pass
+
+
+def Validate_empdata_razao():
+    pass
+
+
+def Validate_empdata_resp():
+    pass
+
+
+def Validate_empdata_tel():
+    pass
+
+
+def Validate_end_bairro():
+    pass
+
+
+def Validate_end_cep():
+    pass
+
+
+def Validate_end_cidade():
+    pass
+
+
+def Validate_end_comp():
+    pass
+
+
+def Validate_end_num():
+    pass
+
+
+def Validate_end_ref():
+    pass
+
+
+def Validate_end_rua():
+    pass
+
+
+def Validate_end_uf():
+    pass
