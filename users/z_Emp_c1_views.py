4cd78b23ee5e15ed903fd17249d244bb36c9ddec
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-10-04 11:23:30 -0300

    add many views

--- users/z_Emp_c1_views.py
+++ users/z_Emp_c1_views.py
@@ -20,6 +20,9 @@ from z__Dicts import Dict_respostas_Session
 
 from z_Emp_c2_methods import NewCompany_Method
 from z_Emp_c2_methods import ListCompany_Method
+from z_Emp_c2_methods import DeleteCompany_Method
+from z_Emp_c2_methods import ActiveCompany_Method
+
 
 @api_view(['POST'])
 def NewEmp_View(request):
@@ -94,7 +97,119 @@ def ListEmpActive_View ( request ):
 
     resptofront.append ( respSess )
     resptofront.extend ( respMeth )
+
+    return Response ( resptofront )
+
+@api_view(['POST'])
+def ListEmpDesactive_View ( request ):
+    resptofront = []
+    respSess  = 'inicializado'
+
+    if request.method == 'POST':      
+        try:
+            sessaoUser   = R_GetTokenfromClient_Interface ( request.data[0] )
+            sessaoValida = ValidSession_Method ( sessaoUser )
+
+            if sessaoValida:
+                
+                nivelValido = True # Todos os níveis tem permissão para visualizar
+
+                if nivelValido:
+                    respSess = 'sucesso'
+                    respMeth = ListCompany_Method   ( request.data[1], False )
+                
+                else:
+                    respSess = 'acessoNegado'    
+            
+            else:
+                respSess = 'sessaoInvalida'        
+        except:
+            
+            respSess = 'erroDesconhecido'  
+    else:
+        respSess = 'erroDesconhecido' 
     
-    print resptofront
+    respSess = Dict_respostas_Session [ respSess ]
+
+    resptofront.append ( respSess )
+    resptofront.extend ( respMeth )
+    
+    
+    return Response ( resptofront )
+
+@api_view(['POST'])
+def ToDesactive_View ( request ):
+    resptofront = []
+    respSess  = 'inicializado'
+    respPost    = 'inicializado'
+
+    if request.method == 'POST':      
+        try:
+            sessaoUser   = R_GetTokenfromClient_Interface ( request.data[0] )
+            sessaoValida = ValidSession_Method ( sessaoUser )
+
+            if sessaoValida:
+                tipodeconta = RetornNiveisComparation ( sessaoUser.nivel )
+                nivelValido = tipodeconta.Adm or tipodeconta.MeP or tipodeconta.Mod 
+
+                if nivelValido:
+                    respSess = 'sucesso'
+                    respPost = DeleteCompany_Method   ( request.data[1], sessaoUser.ids )
+                    respPost = Dict_respostas_Post [ respPost ]
+                
+                else:
+                    respSess = 'acessoNegado'    
+            
+            else:
+                respSess = 'sessaoInvalida'        
+        except:
+            
+            respSess = 'erroDesconhecido'  
+    else:
+        respSess = 'erroDesconhecido' 
+    
+    respSess = Dict_respostas_Session [ respSess ]
+
+    resptofront.append ( respSess )
+    resptofront.append ( respPost )
+    
+    return Response ( resptofront )
+
+@api_view(['POST'])
+def ToReactive_View ( request ):
+    resptofront = []
+    respSess    = 'inicializado'
+    respPost    = 'inicializado'
+
+    if request.method == 'POST':      
+        try:
+            sessaoUser   = R_GetTokenfromClient_Interface ( request.data[0] )
+            sessaoValida = ValidSession_Method ( sessaoUser )
+
+            if sessaoValida:
+                tipodeconta = RetornNiveisComparation ( sessaoUser.nivel )
+                nivelValido = tipodeconta.Adm or tipodeconta.MeP or tipodeconta.Mod 
+
+                if nivelValido:
+                    respSess = 'sucesso'
+                    respPost = ActiveCompany_Method   ( request.data[1], sessaoUser.ids )
+                    respPost = Dict_respostas_Post [ respPost ]
+                
+                else:
+                    respSess = 'acessoNegado'    
+            
+            else:
+                respSess = 'sessaoInvalida'        
+        except:
+            
+            respSess = 'erroDesconhecido'  
+    else:
+        respSess = 'erroDesconhecido' 
+    
+    respSess = Dict_respostas_Session [ respSess ]
+
+    resptofront.append ( respSess )
+    resptofront.append ( respPost )
+    
     return Response ( resptofront )
 
