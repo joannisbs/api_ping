ce07f5a036c7d3499bccdfd9772e26b300a5bc27
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-10-04 11:21:51 -0300

    emp object with id

--- users/z_Emp_c0_obj.py
+++ users/z_Emp_c0_obj.py
@@ -18,3 +18,25 @@ class NewEmp_Object:
 
         self.cont_On         = '0'
         self.cont_Tot        = '0'
+
+class Emp_Object:
+    def __init__(self):
+        self.id              = ''
+        self.emp_nome        = ''
+        self.empdata_cnpj    = ''
+        self.empdata_email   = ''
+        self.empdata_razao   = ''
+        self.empdata_resp    = ''
+        self.empdata_tel     = ''
+
+        self.end_bairro      = ''
+        self.end_cep         = ''
+        self.end_cidade      = ''
+        self.end_comp        = ''
+        self.end_num         = ''
+        self.end_ref         = ''
+        self.end_rua         = ''
+        self.end_uf          = ''
+
+        self.cont_On         = '0'
+        self.cont_Tot        = '0'
