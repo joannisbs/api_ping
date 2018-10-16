1a57b2fe2be5455ea2e9511d434e4558e095937c
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-10-16 15:09:28 -0300

    alterar tabelas parar utf8

---
+++ apiPing/set_utf_collation.py
@@ -0,0 +1,17 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+from django.db import connection
+
+def init():
+    cursor = connection.cursor()
+    tables = connection.introspection.table_names()
+
+    for table in tables:
+        print "Fixing table: %s" %table
+        sql = "ALTER TABLE %s CONVERT TO CHARACTER SET utf8;" %(table)
+        cursor.execute(sql)
+        print "Table %s set to utf8"%table
+
+print "DONE!"
+
+init()
